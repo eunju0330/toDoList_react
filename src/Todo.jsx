91b46fd0ec32c11df409caa7468c11a986f170a1
eunju <echoe7894@gmail.com>
2025-06-27 14:33:52 +0900

    빌드 후 배포

---
+++ src/Todo.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as solid from '@fortawesome/free-solid-svg-icons'
+import './assets/styles/reset.css';
+import './assets/styles/Todo.css';
+
+function Todo(){
+    return(
+        <main>
+            <input type="text" placeholder="오늘의 할 일을 적어주세요" />
+            <button type="button"><FontAwesomeIcon icon={solid.faPenToSquare} /></button>            
+        </main>
+    )
+}
+
+export default Todo;
